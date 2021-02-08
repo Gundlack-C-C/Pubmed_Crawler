0b00189ce6ed2f6608898a97519cd3c007398484
Felix Gundlack <GundlackFelixDev@gmail.com>
2021-02-08 17:27:54 +0100

    set default item values

--- crawler/items.py
+++ crawler/items.py
@@ -12,13 +12,13 @@ from itemloaders.processors import Join, MapCompose, TakeFirst
 class Article(scrapy.Item):
     _id = scrapy.Field(input_processor=MapCompose(
         str.strip), output_processor=TakeFirst())
-    title = scrapy.Field(input_processor=MapCompose(
+    title = scrapy.Field(default="", input_processor=MapCompose(
         str.strip), output_processor=TakeFirst())
-    short = scrapy.Field(input_processor=MapCompose(
+    short = scrapy.Field(default="", input_processor=MapCompose(
         str.strip), output_processor=TakeFirst())
-    authors = scrapy.Field(input_processor=MapCompose(
+    authors = scrapy.Field(default="", input_processor=MapCompose(
         str.strip), output_processor=TakeFirst())
     url = scrapy.Field(input_processor=MapCompose(
         str.strip), output_processor=TakeFirst())
-    journal = scrapy.Field(input_processor=MapCompose(
+    journal = scrapy.Field(default="", input_processor=MapCompose(
         str.strip), output_processor=TakeFirst())
