4f55238ec0457bb556bb75f99dc9624e41fe2039
Felix Gundlack <GundlackFelixDev@gmail.com>
2021-02-08 10:58:04 +0100

    add handling of non existing folder. Add option to replace existing file

--- crawler/pipelines.py
+++ crawler/pipelines.py
@@ -5,20 +5,27 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from scrapy.exporters import JsonItemExporter, PythonItemExporter
-
+import os
 
 class JSONPipeline(object):
     @ classmethod
     def from_crawler(cls, crawler):
         # This method is used by Scrapy to create your spiders.
         file_settings = crawler.settings.getdict("FILE_SETTINGS")
-        if not file_settings:
-            raise "Database is not configured"
+        if not file_settings or 'out' not in file_settings.keys():
+            raise "FILE_SETTINGS missing or incomplete for JSONPipeline"
 
         path = file_settings['out']
-        return cls(path)
+        overwrite = file_settings['overwrite']
+        return cls(path, overwrite)
 
-    def __init__(self, path="data_export.json"):
+    def __init__(self, path="data_export.json", overwrite=False):
+        if not os.path.exists(os.path.abspath(os.path.dirname(path))):
+            os.makedirs(os.path.abspath(os.path.dirname(path)))
+
+        if overwrite and os.path.isfile(path):
+            os.remove(path)
+            
         self.file = open(path, 'wb')
         self.exporter = JsonItemExporter(
             self.file, encoding='utf-8', ensure_ascii=False)
