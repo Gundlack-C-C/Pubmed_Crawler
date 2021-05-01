2e23ead64885e79c2af4f4d01b2b20f5797394ba
Felix Gundlack <GundlackFelixDev@gmail.com>
2021-05-01 11:26:21 +0200

    fix error of failed call due to robotstxt error

--- crawler/settings.py
+++ crawler/settings.py
@@ -29,7 +29,7 @@ FILE_SETTINGS = {
 #USER_AGENT = 'beer (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 CONCURRENT_REQUESTS = 32
