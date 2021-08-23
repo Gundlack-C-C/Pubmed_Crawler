5e76a7ab68fc9c9b7bf4493aed44f883a51cec54
Felix Gundlack <gundlackfelixdev@gmail.com>
2021-08-23 17:45:40 +0200

    remove unused entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,18 +8,7 @@ printf "Log level : [%s]\n" "$LOG_LEVEL"
 echo "##################"
 echo ""
 
-if [ "$MODE" = "ct" ]; then
-	echo "[ct_gov] Start crawler for ClinicalTrials.gov"
-	if [ $DATA_DIR ]; then
-		echo "Crawling all files from ${DATA_DIR}"
-		rm -rf ./out/data_$DATA_DIR.json
-		scrapy crawl ct_gov -o ./out/data_$DATA_DIR.json -a part=$DATA_DIR
-	else
-		echo "Crawling all available files"
-		rm -rf ./out/data_all.json
-		scrapy crawl ct_gov -o ./out/data_all.json
-	fi
-elif [ "$MODE" = "Worker" ]; then
+if [ "$MODE" = "Worker" ]; then
     echo "Worker Mode"
 	pthon3 app_workermq.py
 elif [ "$MODE" = "Service" ]; then
