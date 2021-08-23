384534b822c3a45c02401d09df9d6d1efa16a94f
Felix Gundlack <gundlackfelixdev@gmail.com>
2021-08-23 17:17:20 +0200

    update rabbitmq environment variables

--- RabbitMQ/Configuration.py
+++ RabbitMQ/Configuration.py
@@ -3,12 +3,12 @@ import os
 EXCHANGE_NAME = os.getenv("RABBITMQ_EXCHANGE_NAME")
 ROUTING = {
     'query-pubmed': {
-        'QUEUE_NAME':  os.getenv("RABBITMQ_QUERY_PUBMED_QUEUE_INPUT")
+        'QUEUE_NAME':  os.getenv("RABBITMQ_CRAWLER_PUBMED_QUEUE_INPUT")
     },
     'query-pubmed-status': {
-        'QUEUE_NAME':  os.getenv("RABBITMQ_QUERY_STATUS_QUEUE_INPUT")
+        'QUEUE_NAME':  os.getenv("RABBITMQ_CRAWLER_STATUS_QUEUE_INPUT")
     },
     'query-status': {
-        'QUEUE_NAME':  os.getenv("RABBITMQ_QUERY_STATUS_QUEUE_INPUT")
+        'QUEUE_NAME':  os.getenv("RABBITMQ_CRAWLER_STATUS_QUEUE_INPUT")
     }
 }
