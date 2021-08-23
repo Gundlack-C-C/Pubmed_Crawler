426faa8a84028b42ebea0fa56738827af3fac3e9
Felix Gundlack <gundlackfelixdev@gmail.com>
2021-08-23 16:11:06 +0200

    fix dockerentrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-MODE=$1
-DATA_DIR=$2
-
-echo "docker-entrypoint.sh executing ..."
+echo "##### Input ######"
+printf "Container Environment: [%s]\n" "$MODE"
+printf "Application name : [%s]\n" "$APPLICATION_NAME"
+printf "Listen Port : [%s]\n" "$PORT"
+printf "Log level : [%s]\n" "$LOG_LEVEL"
+echo "##################"
+echo ""
 
 if [ "$MODE" = "ct" ]; then
 	echo "[ct_gov] Start crawler for ClinicalTrials.gov"
@@ -16,12 +19,17 @@ if [ "$MODE" = "ct" ]; then
 		rm -rf ./out/data_all.json
 		scrapy crawl ct_gov -o ./out/data_all.json
 	fi
-elif [ "$MODE" == "ccc" ]; then
-	echo "[ccc] Start crawler for Studienregister Erlangen"
-	scrapy crawl ccc -o ./out/data_$DATA_DIR.json
+elif [ "$MODE" = "Worker" ]; then
+    echo "Worker Mode"
+	pthon3 app_workermq.py
+elif [ "$MODE" = "Service" ]; then
+    echo "Service Mode"
+	python3 app_server.py
+elif [ "$MODE" = "DEV" ]; then
+    echo "Development Mode - Sleep to connect to container"
+    /bin/sh -c "while sleep 1000; do :; done"
 else
 	echo "Unknown Argument [${MODE}]"
 fi
 
-echo "Output in [./out/data_${DATA_DIR}.json]"
-echo "Fertig!"
+echo "Success!"
